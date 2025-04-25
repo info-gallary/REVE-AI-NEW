65f288cb5c694407b1d236d34e62b1f9fdcd1df8
Nisarg <97960921+info-gallary@users.noreply.github.com>
2025-04-25 12:33:21 +0530

    Update poc_x.py

--- poc_x.py
+++ poc_x.py
@@ -335,7 +335,8 @@ with tab1:
                     
                     agent = Agent(
                         # model=Groq(id="meta-llama/llama-4-scout-17b-16e-instruct"),
-                        model=Groq(id="meta-llama/llama-4-maverick-17b-128e-instruct"),
+                        # model=Groq(id="meta-llama/llama-4-maverick-17b-128e-instruct"),
+                        model=Gemini(id="gemini-2.0-flash-exp"),    
                         tools=[CsvTools(csvs=[temp_path],row_limit=11,read_csvs=True)],
                         markdown=True,
                         show_tool_calls=True,
@@ -372,7 +373,8 @@ with tab1:
                     # First agent to get initial analysis
                     agent = Agent(
                         # model=Groq(id="meta-llama/llama-4-scout-17b-16e-instruct"),
-                        model=Groq(id="meta-llama/llama-4-maverick-17b-128e-instruct"),
+                        # model=Groq(id="meta-llama/llama-4-maverick-17b-128e-instruct"),
+                        model=Gemini(id="gemini-2.0-flash-exp"),    
                         tools=[CsvTools(csvs=[temp_path],row_limit=11,read_csvs=True)],
                         markdown=True,
                         show_tool_calls=True,
@@ -403,7 +405,8 @@ with tab1:
                     report_agent = Agent(
                         name="Academic Paper Researcher",
                         # model=Groq(id="meta-llama/llama-4-scout-17b-16e-instruct"),
-                        model=Groq(id="meta-llama/llama-4-maverick-17b-128e-instruct"),
+                        # model=Groq(id="meta-llama/llama-4-maverick-17b-128e-instruct"),
+                        model=Gemini(id="gemini-2.0-flash-exp"),    
                         role="Research academic papers and scholarly content",
                         tools=[ReasoningTools(
                                 think=True,
@@ -698,7 +701,8 @@ with tab2:
                 net_agent = Agent(
                     name="Medical Imaging Expert",
                     # model=Groq(id="meta-llama/llama-4-scout-17b-16e-instruct"),
-                    model=Groq(id="meta-llama/llama-4-maverick-17b-128e-instruct"),
+                    # model=Groq(id="meta-llama/llama-4-maverick-17b-128e-instruct"),
+                    model=Gemini(id="gemini-2.0-flash-exp"),
                     tools=[ArxivTools()],  
                     context={"pred": pred.content,"notes":notes,"age":age,"gender":gender},
                     add_context=True,
